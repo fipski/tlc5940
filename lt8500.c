55d79b8d4ee25807b6c7dce9db6ebaf90cb6ae82
Andrei Andreyanau <a.andreyanau@sam-solutions.com>
2016-08-03 13:25:37 +0300

    Updating skeleton for basic functions. Eclipse configs etc...
    Signed-off-by: Andrei Andreyanau <a.andreyanau@sam-solutions.com>
    
    	modified:   .cproject
    	modified:   .settings/language.settings.xml
    	modified:   lt8500.c

--- lt8500.c
+++ lt8500.c
@@ -12,6 +12,7 @@
 #include <linux/gpio.h>
 #include <linux/of_gpio.h>
 
+#define FRAME_SIZE						0x49
 #define CMD_SYNC_UPDATE_FRAME			0x00
 #define CMD_ASYNC_UPDATE_FRAME			0x10
 #define CMD_CORR_FRAME					0x20
@@ -22,25 +23,14 @@
 #define CMD_CORR_TOGGLE_FRAME			0x70
 
 typedef struct {
-	__u16	brightness[48];
-	__u8	cmd;
-} lt8500_update_frame;
-
-typedef struct {
-	__u8	cor[48];
-	__u8	cmd;
-} lt8500_cor_frame;
-
-typedef struct {
-	__u8	cor[48];
-	__u8	last_cmd;
-	__u8	flags;
-} lt8500_status_frame;
+	__u8	data[FRAME_SIZE];
+} lt8500_frame;
 
 struct lt8500_dev {
 	struct led_classdev	ldev;
 	struct spi_device	*spi;			// Device handler
 	struct mutex		mutex;
+	struct work_struct	work;
 
 	int					count;			// Number of devices in the chain
 	int					ldi_blank_gpio;	// LDI/Blank GPIO pin
@@ -59,18 +49,15 @@ typedef enum {
  */
 static void lt8500_cmd_output_toggle(struct lt8500_dev *dev, stateOutput state)
 {
-	__u16 data[48];
-	__u16 word;
+	__u8 frame[FRAME_SIZE];
 	__u8 i = 0;
 
-	memset(data, 0x0000, ARRAY_SIZE(data));
+	memset(frame, 0x00, FRAME_SIZE);
 
 	spin_lock(&dev->lock);
 
-	for (i = 0; i < ARRAY_SIZE(data); i++) {
-		word = cpu_to_le16(data[i]);
-		spi_write(dev->spi, (const __u8 *)(word >> 8), 1);
-		spi_write(dev->spi, (const __u8 *)(word & 0xff), 1);
+	for (i = 0; i < FRAME_SIZE - 1; i++) {
+		spi_write(dev->spi, (const __u8 *)&frame[i], 1);
 	}
 
 	if (state == STATE_ENABLED)
@@ -79,6 +66,31 @@ static void lt8500_cmd_output_toggle(struct lt8500_dev *dev, stateOutput state)
 		spi_write(dev->spi, (const __u8 *)CMD_OUTPUT_DISABLE_FRAME, 1);
 
 	spin_unlock(&dev->lock);
+}
+
+static void lt8500_update_frame(struct work_struct *work)
+{
+	struct lt8500_dev *dev = container_of(work, struct lt8500_dev, work);
+
+}
+
+/*
+ * This function will return the number of discovered LT's
+ * connected in a chain.
+ */
+static int lt8500_discover(struct spi_device *spi)
+{
+	__u8 frame[FRAME_SIZE];
+	int ret = 0;
+
+	/*
+	 * Fill frame with the mask: 0x0AAA starting from MSB,
+	 * so it will look like this:
+	 * 0x0A < MSB
+	 * 0xAA < LSB
+	 */
+
+	return ret;
 }
 
 static int lt8500_probe(struct spi_device *spi)
@@ -92,18 +104,18 @@ static int lt8500_probe(struct spi_device *spi)
 		return -ENODEV;
 	}
 
-	ltdev = devm_kzalloc(&spi->dev, sizeof(*ltdev), GFP_KERNEL);
-
 	/*
 	 * Set the amount of bits to be transferred at once
 	 */
 	spi->bits_per_word = 8;
+
 
 	return ret;
 }
 
 static int lt8500_remove(struct spi_device *spi)
 {
+	printk("Remove success!");
 	return 0;
 }
 
@@ -114,7 +126,6 @@ static struct spi_driver lt8500_driver = {
 		.probe = lt8500_probe,
 		.remove = lt8500_remove,
 };
-
 module_spi_driver(lt8500_driver);
 
 MODULE_LICENSE("GPL v2");
