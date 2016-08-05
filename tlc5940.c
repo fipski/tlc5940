47348c60f0003377eb8d873beaca838b9ec92329
Andrei Andreyanau <a.andreyanau@sam-solutions.com>
2016-08-05 12:40:55 +0300

    Added reading and setting XLAT pin
    Signed-off-by: Andrei Andreyanau <a.andreyanau@sam-solutions.com>
    
    	modified:   tlc5940.c

--- tlc5940.c
+++ tlc5940.c
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016
+ * Andrei Andreyanau <a.andreyanau@sam-solutions.com
+ *
+ * Based on:
+ * 	- leds-tlc5940.c by Jordan Yelloz <jordan@yelloz.me>
+ * 	- leds-dac124s085.c by Guennadi Liakhovetski <lg@denx.de>
+ *
+ * 	This file is subject to the terms and conditions of version 2 of
+ * 	the GNU General Public License. See the file LICENSE in the main
+ * 	directory of this archive for more details.
+ *
+ * 	LED driver for the TLC5940 SPI LED Controller
+ */
+
 #include <linux/init.h>
 #include <linux/string.h>
 #include <linux/types.h>
@@ -17,65 +32,32 @@
 #include <linux/of_device.h>
 
 #define DRIVER_NAME						"tlc5940"
-#define OF_MAX_CHAIN_SZ					"chain-sz"
-#define FRAME_SIZE						0x49
-//#define CMD_SYNC_UPDATE_FRAME			0x00
-//#define CMD_ASYNC_UPDATE_FRAME			0x10
-//#define CMD_CORR_FRAME					0x20
-//#define CMD_OUTPUT_ENABLE_FRAME			0x30
-//#define CMD_OUTPUT_DISABLE_FRAME		0x40
-//#define CMD_SELF_TEST_FRAME				0x50
-//#define CMD_PHASE_SHIFT_TOGGLE_FRAME	0x60
-//#define CMD_CORR_TOGGLE_FRAME			0x70
+#define OF_MAX_CHAIN_SZ					"chain-sz"	// Number of connected devices from the device tree
+#define OF_XLAT_BLANK					"gpio-xlat"	// Latch for loading the data into the output register
 
-typedef struct {
-	__u8	data[FRAME_SIZE];
-} tlc5940_frame;
+#define TLC5940_MAX_CHAIN_SZ			4096		// Used to limit the number of loop cycles when performing the discovery
+#define TLC5940_SPI_MAX_SPEED			30000000	// Maximum possible SPI speed for the device
+#define TLC5940_SPI_BITS_PER_WORD		8			// Word width
+#define TLC5940_DEV_MAX_LEDS			16			// Maximum number of leds per device
+#define TLC5940_GS_CHANNEL_WIDTH		12			// Grayscale PWM Control resolution (bits)
 
-struct tlc5940_dev {
-	struct spi_device	*spi;			// SPI Device handler
-	__u32				chain_sz;		// Number of devices in the chain
-	int					ldi_blank_gpio;	// LDI/Blank GPIO pin
+struct tlc5940_led {
+	struct led_classdev	ldev;
+	int					id;
+	int					brightness;
+	const char			*name;
+	struct tcl5940_dev	*tlc;
+
+	spinlock_t			lock;
 };
 
-typedef enum {
-	STATE_DISABLED,
-	STATE_ENABLED
-} stateOutput;
+struct tlc5940_dev {
+	struct tlc5940_led	leds[TLC5940_DEV_MAX_LEDS];
 
-/*
- * TODO: pass the amount of LT's in the chain
- */
-//static void lt8500_cmd_output_toggle(struct lt8500_dev *dev, stateOutput state)
-//{
-//	__u8 frame[FRAME_SIZE];
-//	__u8 i = 0;
-//
-//	memset(frame, 0x00, FRAME_SIZE);
-//
-//	spin_lock(&dev->lock);
-//
-//	for (i = 0; i < FRAME_SIZE - 1; i++) {
-//		spi_write(dev->spi, (const __u8 *)&frame[i], 1);
-//	}
-//
-//	if (state == STATE_ENABLED)
-//		spi_write(dev->spi, (const __u8 *)CMD_OUTPUT_ENABLE_FRAME, 1);
-//	else
-//		spi_write(dev->spi, (const __u8 *)CMD_OUTPUT_DISABLE_FRAME, 1);
-//
-//	gpio_set_value(dev->ldi_blank_gpio, 1);
-//	// TODO: Here should be a delay less than 5us
-//	gpio_set_value(dev->ldi_blank_gpio, 0);
-//
-//	spin_unlock(&dev->lock);
-//}
-
-//static void lt8500_update_frame(struct work_struct *work)
-//{
-//	struct lt8500_dev *dev = container_of(work, struct lt8500_dev, work);
-//
-//}
+	struct spi_device	*spi;			// SPI Device handler
+	__u32				chain_sz;		// Number of devices in the chain
+	int					xlat_gpio;		// Latch
+};
 
 /*
  * This function will return the number of discovered LT's
@@ -120,37 +102,74 @@ MODULE_DEVICE_TABLE(of, tlc5940_of_match);
 static int tlc5940_probe(struct spi_device *spi)
 {
 	struct device_node *np = spi->dev.of_node;
-	struct tlc5940_dev *ltdev;
+	struct tlc5940_dev *tlcdev;
 	__u32 count;
+	int latch;
 	int ret = 0;
 
 	if (!np) {
-		printk(KERN_ERR "No platform data specified");
+		printk(KERN_ERR "tlc5940: No platform data specified");
 		return -ENODEV;
 	}
 
-	ltdev = devm_kzalloc(&spi->dev, sizeof(*ltdev), GFP_KERNEL);
-	if (!ltdev)
+	tlcdev = devm_kzalloc(&spi->dev, sizeof(*tlcdev), GFP_KERNEL);
+	if (!tlcdev)
 		return -ENOMEM;
 
+	tlcdev->spi = spi;
+
+	if (tlcdev->spi->max_speed_hz > TLC5940_SPI_MAX_SPEED) {
+		dev_warn(&tlcdev->spi->dev, "spi max speed (%u) is too high, setting to default %u",
+				tlcdev->spi->max_speed_hz, TLC5940_SPI_MAX_SPEED);
+		tlcdev->spi->max_speed_hz = TLC5940_SPI_MAX_SPEED;
+	}
+
 	/*
-	 * Get the number of
+	 * Get the number of connected device from the device tree structure
+	 * and then scan and compare to actual number of connected devices.
 	 */
 	if (of_match_device(of_match_ptr(tlc5940_of_match), &spi->dev)) {
 		if (!of_property_read_u32(np, OF_MAX_CHAIN_SZ, &count)) {
-			ltdev->chain_sz = count;
-			printk(KERN_INFO "%s = %d", OF_MAX_CHAIN_SZ, ltdev->chain_sz);
+			tlcdev->chain_sz = count;
 		}
+	} else {
+		// TODO: call scan function to obtain the number of connected devices
+	}
+
+	/*
+	 * Get XLAT pin
+	 */
+	if (of_match_device(of_match_ptr(tlc5940_of_match), &spi->dev)) {
+		latch = of_get_named_gpio(np, OF_XLAT_BLANK, 0);
+		if (gpio_is_valid(latch)) {
+			if (devm_gpio_request(&spi->dev, latch, OF_XLAT_BLANK)) {
+				dev_err(&tlcdev->spi->dev, "unable to request gpio for XLAT pin");
+				return -EINVAL;
+			}
+			tlcdev->xlat_gpio = latch;
+		} else {
+			dev_err(&tlcdev->spi->dev, "specified gpio pin for XLAT is invalid");
+			return -EINVAL;
+		}
+	}
+
+	// Set the direction of XLAT pin
+	ret = gpio_direction_output(tlcdev->xlat_gpio, 1);
+	if (ret) {
+		dev_err(&tlcdev->spi->dev, "Failed to configure XLAT pin for output: %d\n", ret);
+
+		return ret;
 	}
 
 	/*
 	 * Set the amount of bits to be transferred at once
 	 */
-	spi->bits_per_word = 8;
+	spi->bits_per_word = TLC5940_SPI_BITS_PER_WORD;
 
 	// TODO: Init locks
 
-	printk(KERN_INFO "TI TLC5940 SPI device registered");
+	dev_info(&tlcdev->spi->dev, "TI tlc5940 SPI driver registered");
+
 	return ret;
 }
 
@@ -171,7 +190,7 @@ static struct spi_driver tlc5940_driver = {
 };
 module_spi_driver(tlc5940_driver);
 
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Andrei Andreyanau <a.andreyanau@sam-solutions.com>");
 MODULE_DESCRIPTION("TI TLC5940 driver");
-MODULE_ALIAS("spi:tlc5940");
+MODULE_ALIAS("spi:" DRIVER_NAME);
